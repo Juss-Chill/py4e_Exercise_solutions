1e8c111bc25f4b1e68af3194cf444a6faede7fba
Muni <srinivasmuni23@gmail.com>
2022-08-02 19:19:34 +0530

    Exercise 5 sol

---
+++ Chapter_2/exercise_2_5.py
@@ -0,0 +1,8 @@
+tempC = input("Enter the temperature in Celsius: ")
+#convert string to float
+tempC = float(tempC)
+
+#convert the temp to Fahrenheit
+tempF = (1.8 * tempC) + 32
+
+print(tempC,"celsisus is equal to",tempF, "in FahrenHeit")
